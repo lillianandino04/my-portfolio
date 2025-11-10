712231466461025082b71b488d026774d5f4c16f
Lillian Andino <lillianandino04@gmail.com>
2025-11-10 00:26:41 -0600

    base vite config fix attempt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './', 
+  base: '/my-portfolio/', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
