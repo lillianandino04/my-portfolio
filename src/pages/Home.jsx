4f1bfd3a5556efdec97418fb602b097cf0f9780f
Lillian Andino <lillianandino04@gmail.com>
2026-01-17 11:24:34 -0600

    Fix formatting of resume download link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,8 +42,8 @@ export const Home = () => {
                         </h2>
                         {/*neon-command-button*/}
                         <a 
-                            href="/Lillian_Andino_Granillo_Resume.pdf" //path
-                            download="Lillian_Andino_Granillo_Resume.pdf" //filename for download
+                            href="/Lillian_Andino_Resume.pdf" //path
+                            download="Lillian_Andino_Resume.pdf" //filename for download
                             className="neon-command-button inline-block" //make <a> behave like a button
                         >
                             DOWNLOAD MY RESUME HERE
